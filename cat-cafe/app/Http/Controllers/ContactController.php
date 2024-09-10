f9f456caf7aef1e1d3dbc870dabffc971e4859dd
hiroka <mitsunagahrk87@gmail.com>
2024-09-10 09:20:48 +0900

    section12 35

--- cat-cafe/app/Http/Controllers/ContactController.php
+++ cat-cafe/app/Http/Controllers/ContactController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ContactRequest;
-use \Illuminate\Support\Facades\Log;
+use \Illuminate\Support\Facades\Mail;
+use App\Mail\ContactAdminMail;
 
 class ContactController extends Controller
 {
@@ -13,10 +14,12 @@ class ContactController extends Controller
 
     function sendMail(ContactRequest $request) {
         $validated = $request->validated();
-    
+        
+        Mail::to('mitsunagahrk87@gmail.com')->send(new ContactAdminMail());
         // これ以降の行は入力エラーがなかった場合のみ実行されます
         // 登録処理(実際はメール送信などを行う)
-        Log::debug($validated['name']. 'さんよりお問い合わせがありました');
+        // Log::debug($validated['name']. 'さんよりお問い合わせがありました');
+
         return to_route('contact.complete');
     }
     public function complete(){
