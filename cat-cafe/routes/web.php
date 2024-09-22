0e515ec9212a7c6c3409b8660c69d54971f62216
hiroka <mitsunagahrk87@gmail.com>
2024-09-22 12:08:58 +0900

    section14 41

--- cat-cafe/routes/web.php
+++ cat-cafe/routes/web.php
@@ -1,14 +1,29 @@
 <?php
 
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\Admin\AdminBlogController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
 //     return view('index');
 // });
 
-Route::view('/', 'index' );
+Route::view('/', 'index');
 
+//問い合わせ内容
 Route::get('/contact', [ContactController::class, 'index'])->name('contact');
-Route::post('contact',[ContactController::class, 'sendMail']);
-Route::get('/contact/complete', [ContactController::class, 'complete'])->name('contact.complete');
+Route::post('contact', [ContactController::class, 'sendMail']);
+Route::get('/contact/complete', [ContactController::class, 'complete'])->name(
+    'contact.complete',
+);
+
+//ブログ
+Route::get('/admin/blogs', [AdminBlogController::class, 'index'])->name(
+    'admin.blogs.index',
+);
+Route::get('/admin/blogs/create', [AdminBlogController::class, 'create'])->name(
+    'admin.blogs.create',
+);
+Route::post('admin/blogs', [AdminBlogController::class, 'store'])->name(
+    'admin.blogs.store',
+);
